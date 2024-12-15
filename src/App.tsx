80cce32a65aab23b3e25675ef6ae08b91ed92495
nblancoz <nblancozappala@gmail.com>
2024-12-15 09:23:32 +0100

    tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 export default function App() {
   return (
     <>
-      <p>app</p>
+      <div className="bg-blue-500 text-white text-center p-10">
+        <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
+        <p className="text-lg">Esto es un test de Tailwind CSS.</p>
+      </div>
+      <div className="bg-green-500 text-white">
+        Este es un contenedor verde.
+      </div>
     </>
   );
 }
