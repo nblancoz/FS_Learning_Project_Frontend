67dbdbc4bcfca1cb5a1390a041334efa21176dbd
nblancoz <nblancozappala@gmail.com>
2024-12-17 16:20:40 +0100

    prettier added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 export default function App() {
   return (
     <>
-      <div className="bg-blue-500 text-white text-center p-10">
-        <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
-        <p className="text-lg">Esto es un test de Tailwind CSS.</p>
+      <div className='bg-blue-500 text-white text-center p-10'>
+        <h1 className='text-3xl font-bold'>Hello, Tailwind CSS!</h1>
+        <p className='text-lg'>Esto es un test de Tailwind CSS.</p>
       </div>
-      <div className="bg-green-500 text-white">
+      <div className='bg-green-500 text-white'>
         Este es un contenedor verde.
       </div>
     </>
