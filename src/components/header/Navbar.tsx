4f3b6efae733be3413cdcf7d46f44cda91aab507
nblancoz <nblancozappala@gmail.com>
2024-12-17 18:04:08 +0100

    navbar ui done

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -5,7 +5,7 @@ import { ShoppingBagIcon } from '@heroicons/react/24/outline';
 import useNavbarLogic from './useNavbarLogic';
 
 export default function Navbar() {
-  const { navigation } = useNavbarLogic();
+  const { navigation, search } = useNavbarLogic();
   //   const [open, setOpen] = useState(false);
 
   return (
@@ -20,16 +20,6 @@ export default function Navbar() {
         >
           <div className='border-b border-gray-300'>
             <div className='flex h-16 items-center'>
-              <button
-                type='button'
-                // onClick={() => setOpen(true)}
-                className='relative rounded-md bg-white p-2 text-gray-400 lg:hidden'
-              >
-                <span className='absolute -inset-0.5' />
-                <span className='sr-only'>Open menu</span>
-                <Bars3Icon aria-hidden='true' className='size-6' />
-              </button>
-
               <div className='ml-4 flex lg:ml-0'>
                 <a href='#'>
                   <span className='sr-only'>FortiFit Store</span>
@@ -66,37 +56,53 @@ export default function Navbar() {
                   </a>
                 </div>
 
-                {/* change currency */}
+                {/* WIP selector */}
                 {/* <div className='hidden lg:ml-8 lg:flex'>
-                  <a
-                    href='#'
-                    className='flex items-center text-gray-700 hover:text-gray-800'
+                  <select
+                    className='block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+                    defaultValue={currencies[0].name}
                   >
-                    <img
-                      alt=''
-                      src='https://tailwindui.com/plus/img/flags/flag-canada.svg'
-                      className='block h-auto w-5 shrink-0'
-                    />
-                    <span className='ml-3 block text-sm font-medium'>CAD</span>
-                    <span className='sr-only'>, change currency</span>
-                  </a>
+                    {currencies.map((currency) => (
+                      <option key={currency.name} value={currency.name}>
+                        {currency.name}
+                      </option>
+                    ))}
+                  </select>
                 </div> */}
 
-                <div className='flex lg:ml-6'>
-                  <a href='#' className='p-2 text-gray-400 hover:text-gray-500'>
-                    <span className='sr-only'>Search</span>
+                <div className='ml-4 flex items-center space-x-2'>
+                  <input
+                    type='text'
+                    placeholder='Busca Aqui'
+                    className='block min-w-0 grow py-1.5 pl-3 pr-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm'
+                    onKeyDown={(e) => {
+                      if (e.key === 'Enter') {
+                        const query = (e.target as HTMLInputElement).value;
+                        search(query);
+                      }
+                    }}
+                  />
+                  <button
+                    onClick={() => {
+                      const query = (
+                        document.querySelector('input') as HTMLInputElement
+                      ).value;
+                      search(query);
+                    }}
+                    className='p-1 text-gray-400 hover:text-gray-500'
+                  >
                     <MagnifyingGlassIcon
+                      className='h-5 w-5'
                       aria-hidden='true'
-                      className='size-6'
                     />
-                  </a>
+                  </button>
                 </div>
 
                 <div className='ml-4 flow-root lg:ml-6'>
                   <a href='#' className='group -m-2 flex items-center p-2'>
                     <ShoppingBagIcon
                       aria-hidden='true'
-                      className='size-6 shrink-0 text-gray-400 group-hover:text-gray-500'
+                      className='size-5 shrink-0 text-gray-400 group-hover:text-gray-500'
                     />
                     <span className='ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800'>
                       0
--- src/components/header/useNavbarLogic.ts
+++ src/components/header/useNavbarLogic.ts
@@ -1,4 +1,12 @@
+import { useState } from 'react';
+
 export default function useNavbarLogic() {
+  const [isSearchVisible, SetIsSearchVisible] = useState(false);
+
+  const toggleSearch = () => {
+    SetIsSearchVisible(!isSearchVisible);
+  };
+
   const navigation = [
     {
       name: 'Inicio',
@@ -14,5 +22,25 @@ export default function useNavbarLogic() {
     },
   ];
 
-  return { navigation };
+  const currencies = [
+    {
+      name: 'COP',
+      symbol: '$',
+      img: 'https://restcountries.com/data/col.svg',
+    },
+    {
+      name: 'USD',
+      symbol: 'US$',
+      img: 'https://restcountries.com/data/usa.svg',
+    },
+  ];
+
+  const search = async (query: string) => {
+    if (!query.trim()) {
+      return;
+    }
+    console.log(query);
+  };
+
+  return { isSearchVisible, toggleSearch, navigation, currencies, search };
 }
